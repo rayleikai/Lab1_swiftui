ffc7f989abe48b57779ad1938b683c68f300f693
Huang Sheng Hsin <kevinhang2003@gmail.com>
2021-02-25 10:24:55 +0800

    Initial Commit

---
+++ lab1/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  lab1
+//
+//  Created by YuTech x CBIC on 2021/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceup: false)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceup: Bool
+    var body: some View {
+        ZStack {
+            if isFaceup {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("😈")
+            } else {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by YuTech x CBIC on 2021/2/24.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
